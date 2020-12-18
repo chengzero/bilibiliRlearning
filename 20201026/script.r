0908c02dae71a9d15ca8518bed0a12f4f1e8354a
chengzero <76196084+chengzero@users.noreply.github.com>
2020-12-18 16:26:05 +0800

    Update script.r

--- 20201026/script.r
+++ 20201026/script.r
@@ -1,24 +1,24 @@
 dat <- read.table('datforfor.txt', header = TRUE, sep = '\t')
 
-plotdat <- dat[,1:4]
-names(plotdat)[4] <- 'rmse'
-plotdat$model <- names(dat)[4]
+plotdat <- dat[,1:4] #提取前4列
+names(plotdat)[4] <- 'rmse' #改列名字 
+plotdat$model <- names(dat)[4] #增加一列变量，并使其值等于列名字：字符、现在一共5列
 
-plotdat2 <- dat[,c(1:3, 5)]
+plotdat2 <- dat[,c(1:3, 5)]#构建向量提取4列、将5变成i
 names(plotdat2)[4] <- 'rmse'
-plotdat2$model <- names(dat)[5]
-plotdat3 <- rbind(plotdat, plotdat2)
+plotdat2$model <- names(dat)[5]#也是5列
+plotdat3 <- rbind(plotdat, plotdat2)#按照行合并
 
 
 plotdat <- dat[,1:4]
 names(plotdat)[4] <- 'rmse'
-plotdat$model <- names(dat)[4]
+plotdat$model <- names(dat)[4]#plotdat一共5列
 for(i in 5:9){print(i)
   onedat <- dat[,c(1:3, i)]
   names(onedat)[4] <- 'rmse'
-  onedat$model <- names(dat)[i]
+  onedat$model <- names(dat)[i]#将model下的值等于列名字
   plotdat <- rbind(plotdat, onedat)
-  }
+  }#for(循环条件){循环脚本}
 
 library(ggplot2)
 ggplot(plotdat, aes(x = MLR.R2, y = rmse, colour = model, shape = model)) + geom_point(size = 3)
